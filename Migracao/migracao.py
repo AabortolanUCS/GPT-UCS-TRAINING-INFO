9abdc401da2f317de0b5319964136cfa0dfe51fd
AabortolanUCS <aabortolan@ucs.br>
2023-09-17 19:24:44 -0300

    Migracao dados

---
+++ Migracao/migracao.py
@@ -0,0 +1,26 @@
+import csv
+import os
+import pathlib
+
+def my_function(path, pasta, arquivo):
+  
+  print(arquivo)
+  with open(path+"\\"+pasta+"\\"+arquivo, 'r') as file, open('.\\Dados-Migrados\\dados.jsonl','a') as migrateFile:
+        csvreader = csv.reader(file)
+        for row in csvreader:
+            migrateFile.write("{\"prompt\": \"" + row[0] + "\", \"completion\": \"" + row[1] + "\"}\n")
+            if(arquivo == "Doutorado Acadêmico em Educação.csv"):
+                print(row[0] + " "+ row[1])
+
+
+if pathlib.Path(".\\Dados-Migrados\\dados.jsonl").is_file():
+    os.remove(".\\Dados-Migrados\\dados.jsonl")
+
+path = ".\\Dados"
+dir_list = os.listdir(path)
+
+for pasta in dir_list:
+    subdir_list = os.listdir(path+"\\"+pasta)
+    for arquivo in subdir_list:
+        my_function(path, pasta, arquivo)
+        
